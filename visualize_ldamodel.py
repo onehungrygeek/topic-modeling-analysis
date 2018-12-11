fef46a6ec1b8ebced5169c0988f238724a3ec0ff
Akshay Kulkarni <akshaykulkarni19081994@gmail.com>
2018-12-10 22:55:58 -0500

    Code reformat. Added docstring and comments.

--- visualize_ldamodel.py
+++ visualize_ldamodel.py
@@ -1,5 +1,8 @@
+# Library imports
+# warnings module to surpress all FutureWarnings
 import warnings
-warnings.filterwarnings(action='ignore', category=FutureWarning, module='pyLDAvis')
+warnings.filterwarnings(
+    action='ignore', category=FutureWarning, module='pyLDAvis')
 warnings.filterwarnings(action='ignore', category=FutureWarning)
 import os
 import time
@@ -8,9 +11,21 @@ import pyLDAvis.gensim
 
 
 def visualize(model, corpus, id2word, time_string):
+    """
+    This module uses pyLDAvis library for LDA visualization
+
+    Arguments:
+        model {LdaModel} -- LdaModel computed from gensim.model.ldamodel.LdaModel
+        corpus {list} -- List of corpus created from preprocessed texts
+        id2word {Dictionary} -- Dictionary of ids with mapped words
+        time_string {str} -- Current timestamp
+    """
+
+    # Get current working directory and save visualization to Output_Files directory
     current_dir = os.getcwd()
-    # time_string = time.strftime("%m-%d-%Y_%H-%M-%S")
+    # Visualize
     vis = pyLDAvis.gensim.prepare(model, corpus, id2word)
-    file = current_dir + '/Output_Files/LDA_vis_' + time_string + '.html'
-    pyLDAvis.save_html(vis, file)
-    print('\nSaving LDA model visualization to: ', file)
+    # Save visualization into file_name
+    file_name = current_dir + '/Output_Files/LDA_vis_' + time_string + '.html'
+    pyLDAvis.save_html(vis, file_name)
+    print('\nSaving LDA model visualization to: ', file_name)

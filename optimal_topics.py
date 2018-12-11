04c5c1d48ca8e9446f559e07a872f0946f7723f1
Akshay Kulkarni <akshaykulkarni19081994@gmail.com>
2018-12-11 02:35:12 -0500

    Code reformat. Added docstring and comments.

--- optimal_topics.py
+++ optimal_topics.py
@@ -1,10 +1,21 @@
-from pprint import pprint
 import numpy as np
+from pprint import pprint
 
 
 def print_optimal_topics(model_list, all_scores):
+    """
+    This module selects a model for the list with maximum coherence score and prints the topics for that model.
+
+    Arguments:
+        model_list {list} -- List of computed models
+        all_scores {list} -- List of coherence scores for respective computed models
+    """
+
+    # Index of the maximum coherence score
     optimal_idx = np.argmax(all_scores)
+    # Optimal model for above index
     optimal_model = model_list[optimal_idx]
-    optimal_model_topics = optimal_model.show_topics(formatted=False)
-    # Can pass num_words = 10 to below print_topics() function
+    # You can change number of words per topic by passing
+    # num_words argument to print_topics() function below.
+    # For example, pprint(optimal_model.print_topics(num_words=20))
     pprint(optimal_model.print_topics())

f31a68472837a06834a40f88ea9c34ecb236f9fb
Akshay Kulkarni <akshaykulkarni19081994@gmail.com>
2018-12-29 00:23:07 -0500

    Config changed from config_file.json to config.py file

---
+++ config.py
@@ -0,0 +1,11 @@
+# Parameters you can tweak
+num_topics = 5                  # Default: 5
+num_words = 10                  # Default: 10
+max_topics = 50                 # Default: 50
+measure = 'c_v'                 # Default: 'c_v'
+random_state = 100              # Default: 100
+update_every = 1                # Default: 1
+chunksize = 100                 # Default: 100
+passes = 10                     # Default: 10
+alpha = 'auto'                  # Default: 'auto'
+per_word_topics = True          # Default: True

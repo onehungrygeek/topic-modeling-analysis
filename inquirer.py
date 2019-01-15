418a667a7aa33d4846c04be2a7d28a0d7659d7b4
Akshay Kulkarni <akshaykulkarni19081994@gmail.com>
2019-01-14 20:11:29 -0500

    Added comments and docstring. Code reformat using 'black'

--- inquirer.py
+++ inquirer.py
@@ -1,3 +1,4 @@
+# Library imports
 import os
 import sys
 import glob
@@ -5,6 +6,7 @@ import pickle
 from pprint import pprint
 from PyInquirer import style_from_dict, Token, prompt
 
+# Style dictionary for the PyInquirer module
 style = style_from_dict(
     {
         Token.Separator: "#cc5454",
@@ -17,18 +19,27 @@ style = style_from_dict(
     }
 )
 
+# Get current directory
 current_directory = os.getcwd()
 
+# Initialize variables (necessary for PyInquirer)
 model, topics, dataset = None, None, None
 
+# Get models directory and list all available pickles
+# in the order of most recently modified files first
+# Only recent 8 are displayed. Enter the exact name if it is not in top 8
 model_files_directory = os.path.join(current_directory, "Saved_Models/")
 model_pickles = glob.glob(model_files_directory + "*.pkl")
 model_pickles.sort(key=lambda x: os.path.getmtime(x), reverse=True)
 
+# Get dataset directory and list all available pickles
+# in the order of most recently modified files first
+# Only recent 8 are displayed. Enter the exact name if it is not in top 8
 dataset_files_directory = os.path.join(current_directory, "Datasets/")
 dataset_files = glob.glob(dataset_files_directory + "*.txt")
 dataset_files.sort(key=lambda x: os.path.getmtime(x), reverse=True)
 
+# Questions to be asked by PyInquirer
 questions = [
     {
         # Default
@@ -38,11 +49,11 @@ questions = [
         "choices": [
             "Run topic modeling analysis on a dataset.",
             "Load already saved model and topics.",
-            "Exit"
+            "Exit",
         ],
     },
     {
-        # Number of words to output
+        # Number of words in output
         "type": "input",
         "name": "num_words",
         "message": "Enter number of words to output from this model:",
@@ -70,6 +81,18 @@ questions = [
 
 
 def file_error_checker(file):
+    """
+    Module to check errors with the input file. It checks 
+    if the file does not exist and also if the file is empty.
+
+    Arguments:
+        file {str} -- Complete path of the input file
+
+    Raises:
+        SystemExit -- If file does not exist
+        SystemExit -- If file exists but is empty
+    """
+
     if not os.path.isfile(file):
         raise SystemExit(
             "\nERROR: File not found. Please check again!\n\nScript exited..."
@@ -81,6 +104,13 @@ def file_error_checker(file):
 
 
 def model_pickle_loader():
+    """
+    This module loads pickled models and returns an actual gensim model object.
+
+    Returns:
+        gensim.model.MODEL -- A gensim model object
+    """
+
     choice_number = 2
     directory = model_files_directory
 
@@ -104,6 +134,13 @@ def model_pickle_loader():
 
 
 def dataset_chooser():
+    """
+    This module lists all available datasets stored in the Datasets directory to run analysis
+
+    Returns:
+        str -- Complete path of the dataset file selected by the user
+    """
+
     prompter = prompt(questions[3], style=style)
     choice = prompter["dataset"]
     if choice == "Enter name instead...":
@@ -114,6 +151,13 @@ def dataset_chooser():
 
 
 def inquire():
+    """
+    This module starts the inquiry process and handles various exceptions
+
+    Returns:
+        str -- Complete path of the selected dataset if dataset option is chosen, else prints topics
+    """
+
     print()
     prompter = prompt(questions[0], style=style)
     if prompter["default"] == questions[0]["choices"][0]:
